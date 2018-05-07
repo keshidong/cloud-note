01225f0043ca8008f5a5ba1ebf87c631b06e5648
keshidong <keshidong.whu@gmail.com>
2018-05-07 18:33:41 +0800

    MOD: 更改为nebPay发送交易

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ import { Tabs, Tab } from 'material-ui/Tabs';
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import { fade } from 'material-ui/utils/colorManipulator';
 import SubmitNote from './components/SubmitNote';
-import SendTransactionConfirm from './components/SendTransactionConfirm';
 import NoteRetrieval from './components/NoteRetrieval';
+import NebPay from './libs/nebPay';
 import { Neb, HttpRequest } from 'nebulas';
 import AppInfo from './components/AppInfo';
 import config from './config';
 
 
 const neb = new Neb();
+const nebPay = new NebPay();
 const { contact_address, contact_host } = config;
 // neb.setRequest(new HttpRequest("https://testnet.nebulas.io"));
 
 neb.setRequest(new HttpRequest(contact_host));
-
 
 const muiTheme = getMuiTheme({
     palette: {
@@ -45,94 +45,34 @@ const muiTheme = getMuiTheme({
 
 class App extends Component {
     state = {
-        ifAccountDialogOpen: false,
         noteList: [],
     }
     handleSubmitNote = ({ title, content }) => {
-        this.setState({
-            ifAccountDialogOpen: true,
-        });
-        this.handleSendTransaction = ({ address, passphrase }) => {
-            // send transaction
-            // todo
-            neb.api.getAccountState(address).then(function (state) {
-                console.log(state);
-                return state;
-            })
-                .catch(function (err) {
-                    console.log("err:",err);
-                })
-                .then((state) => {
-                    const { nonce } = state;
-                    return neb.admin.sendTransactionWithPassphrase({
-                        from: address,
-                        to: contact_address,
-                        value: 0,
-                        contract: {
-                            "function": "save",
-                            "args": JSON.stringify([title, content, new Date().toString(), ""])
-                        },
-                        nonce: Number(nonce) + 1,
-                        gasPrice: 1000000,
-                        gasLimit: 2000000,
-                        passphrase: passphrase
-                    });
-                })
-                .then(function(tx) {
-                    //code
-                    console.log(tx);
-                });
-                this.setState({
-                    ifAccountDialogOpen: false,
-                });
-        }
-    }
-    handleDialogHide = () => {
-        this.setState({
-            ifAccountDialogOpen: false,
-        });
+        nebPay.call(contact_address, 0, "save", JSON.stringify([title, content, new Date().toString(), ""]),
+            {
+                qrcode: {
+                    showQRCode: true,
+                },
+                //set listener for extension transaction result
+                listener: (resp) => {
+                    console.log(resp);
+                }
+            },
+        );
     }
     handleRetrieval = () => {
-        this.setState({
-            ifAccountDialogOpen: true,
-        });
-        this.handleSendTransaction = ({ address, passphrase }) => {
-            // send transaction
-            // get notes
-            // todo
-            neb.api.getAccountState(address).then(function (state) {
-                console.log(state);
-                return state;
-            })
-                .catch(function (err) {
-                    console.log("err:",err);
-                })
-                .then((state) => {
-                    const { nonce } = state;
-                    return neb.api.call({
-                        from: address,
-                        to: contact_address,
-                        value: 0,
-                        contract: {
-                            "function": "get",
-                            "args": ""
-                        },
-                        nonce: Number(nonce) + 1,
-                        gasPrice: 1000000,
-                        gasLimit: 2000000,
-                        passphrase: passphrase
-                    });
-                })
-                .then((res) => {
-                    //code
-                    const list = JSON.parse(res.result).filter((el) => (el));
-                    this.setState({
-                        noteList: list,
-                        ifAccountDialogOpen: false,
-                    });
+        nebPay.simulateCall(contact_address, 0, 'get', '', {
+            qrcode: {
+                showQRCode: true
+            },
+            listener: (res) => {
+                console.log(res);
+                const list = JSON.parse(res.result).filter((el) => (el));
+                this.setState({
+                    noteList: list,
                 });
-
-        }
+            }
+        });
     }
     render() {
         return (
@@ -154,11 +94,6 @@ class App extends Component {
                             >Keep your IMPORTANT Info</div>
                             <SubmitNote
                                 submit={this.handleSubmitNote}
-                            />
-                            <SendTransactionConfirm
-                                open={this.state.ifAccountDialogOpen}
-                                onClose={this.handleDialogHide}
-                                sendTransaction={this.handleSendTransaction}
                             />
                         </div>
                     </Tab>
